1917f85d34bb0693fc7cf365b09ccf44f96d74e6
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-24 17:52:53 +0300

    created Profile model

--- projects/models.py
+++ projects/models.py
@@ -4,3 +4,10 @@ from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
+
+class Profile(models.Model):
+    photo = CloudinaryField('images')
+    Bio = models.CharField(max_length=30)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    contact=models.CharField(max_length=12,null=True)
+    datecreated= models.DateField(auto_now_add=True )
