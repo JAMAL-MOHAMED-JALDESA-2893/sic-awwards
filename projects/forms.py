6989c5a534acd6431a5d1b8dd38478ccb91c9908
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-24 23:50:43 +0300

    adds class profileserializer and project serializers

--- projects/forms.py
+++ projects/forms.py
@@ -3,26 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Projects,Profile,Revieww,RATE_CHOICES
 
-class profileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = [ 'photo', 'Bio']
 
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-    class Meta:
-        model = User
-        fields = ['username','email'] 
-
-class UpdateUserProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ['Bio', 'photo']
-
-class projectForm(forms.ModelForm):
-    class Meta:
-        model = Projects
-        fields = ['title','description','projectscreenshot','projecturl']
 class RegistrationForm(UserCreationForm):
     email=forms.EmailField()
     class Meta:
@@ -36,6 +17,29 @@ class RegistrationForm(UserCreationForm):
 
             user.save()
         return user
+class profileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = [ 'photo', 'Bio']
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['Bio', 'photo']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username','email'] 
+
+
+class projectForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        fields = ['title','description','projectscreenshot','projecturl']
+
 
 class RateForm(forms.ModelForm):
     
