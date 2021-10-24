126673d09b907cd9ef699dcc93ce796fac6efcfe
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-24 23:14:44 +0300

    added register function

--- projects/views.py
+++ projects/views.py
@@ -18,3 +18,26 @@ from .serializer import ProfileSerializer,ProjectSerializer
 def index(request):
     projects = Projects.objects.all()
     return render(request,'index.html',{"projects":projects})
+
+
+def register(request):
+    if request.method=="POST":
+        form=RegistrationForm(request.POST)
+        procForm=profileForm(request.POST, request.FILES)
+        if form.is_valid() and procForm.is_valid():
+            username=form.cleaned_data.get('username')
+            user=form.save()
+            profile=procForm.save(commit=False)
+            profile.user=user
+            profile.save()
+
+            # messages.success(request, f'Successfully created Account!.You can now login as {username}!')
+        return redirect('login')
+    else:
+        form= RegistrationForm()
+        prof=profileForm()
+    params={
+        'form':form,
+        'profForm': prof
+    }
+    return render(request, 'users/register.html', params)    
