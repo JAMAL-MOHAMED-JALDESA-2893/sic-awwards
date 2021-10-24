552174bc2ba8a19a48bca41afaf15adad71acf20
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-24 22:33:32 +0300

    add class model projects

--- projects/models.py
+++ projects/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+import datetime as dt
 
 
 # Create your models here.
 
 class Profile(models.Model):
-    photo = CloudinaryField('images')
-    Bio = models.CharField(max_length=30)
     user = models.OneToOneField(User,on_delete=models.CASCADE)
+    photo = CloudinaryField('image')
+    Bio = models.CharField(max_length=30)    
     contact=models.CharField(max_length=12,null=True)
     datecreated= models.DateField(auto_now_add=True )
 
@@ -24,3 +25,13 @@ class Profile(models.Model):
 
     def delete_profile(self):
         self.delete()    
+
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=300)
+    projectscreenshot = CloudinaryField('images')
+    projecturl= models.URLField(max_length=200)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, default='', null=True ,related_name='author')
+    datecreated= models.DateField(auto_now_add=True )
