343515d250f577e82ca95f005b5e2266c0e22ef9
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-24 16:03:28 +0300

    add email tls configurations in settings

--- sicawwards/settings.py
+++ sicawwards/settings.py
@@ -63,6 +63,15 @@ DATABASES['default'].update(db_from_env)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
+#email 
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -70,6 +79,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'rest_framework',
     'bootstrap3',
+     'star_ratings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
