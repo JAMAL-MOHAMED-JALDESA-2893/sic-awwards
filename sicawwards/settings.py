a26c30984d436e82e1de4b90c61fe29de7f5d235
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-10-26 11:09:04 +0300

    deployment

--- sicawwards/settings.py
+++ sicawwards/settings.py
@@ -65,11 +65,6 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 #email configurations
 
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 
